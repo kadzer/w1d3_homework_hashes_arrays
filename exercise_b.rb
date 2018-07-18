bfed6e4b9a60970d2a2954e6d043777ab7141275
Kaspars Dzerins <kadzer@gmail.com>
2018-07-18 19:53:24 +0100

    completing task 4... finally

--- exercise_b.rb
+++ exercise_b.rb
@@ -64,6 +64,8 @@ p users["Erik"][:home_town]
 p users["Erik"][:lottery_numbers]
 
 # Get the type of Avril's pet Monty
+p users["Avril"][:pets][0][:species]
+
 # Get the smallest of Erik's lottery numbers
 # Return an array of Avril's lottery numbers that are even
 # Erik is one lottery number short! Add the number 7 to be included in his lottery numbers
