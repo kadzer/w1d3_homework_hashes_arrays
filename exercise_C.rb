97f6e84b64138272e8ace2529f5b4b9130fb1426
Kaspars Dzerins <kadzer@gmail.com>
2018-07-18 23:29:02 +0100

    solving task 2 ex C

--- exercise_C.rb
+++ exercise_C.rb
@@ -20,6 +20,12 @@ united_kingdom = [
 united_kingdom[1][:capital]="Cardiff"
 
 # Create a Hash for Northern Ireland and add it to the united_kingdom array (The capital is Belfast, and the population is 1,811,000).
+n_i = {
+	name: "Northern Ireland",
+	population: 1811000,
+	capital: "Belfast"
+}
+united_kingdom.push(n_i)
 
 # Use a loop to print the names of all the countries in the UK.
 
