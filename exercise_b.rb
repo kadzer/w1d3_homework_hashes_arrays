cb017944f14ec5192b263f23dc3ce95416ca0e80
Kaspars Dzerins <kadzer@gmail.com>
2018-07-18 19:57:54 +0100

    completing task 5

--- exercise_b.rb
+++ exercise_b.rb
@@ -67,6 +67,8 @@ p users["Erik"][:lottery_numbers]
 p users["Avril"][:pets][0][:species]
 
 # Get the smallest of Erik's lottery numbers
+p users["Erik"][:lottery_numbers].sort.first
+
 # Return an array of Avril's lottery numbers that are even
 # Erik is one lottery number short! Add the number 7 to be included in his lottery numbers
 # Change Erik's hometown to Edinburgh
